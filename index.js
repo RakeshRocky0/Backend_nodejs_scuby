05289f1a43dc7a4d51c4f540b0ac52dcf5aaf53c
RakeshRocky0 <167605155+RakeshRocky0@users.noreply.github.com>
2025-09-09 12:09:23 +0530

    made changes for PORT and url with homepage

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const path = require('path');
 
 const app = express();
 
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 
 dotEnv.config();
 
@@ -28,6 +28,6 @@ app.use('/uploads', express.static('uploads'));
 app.listen(PORT,()=>{
     console.log(`Server is running on port ${PORT}`)
 });     
-app.use("/home", (req,res)=>{
+app.use("/", (req,res)=>{
     res.send("<h1>Welcome to the page</h1>");
 });
